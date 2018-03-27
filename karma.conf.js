3108f42ab08a9d73c2c01c3170cd93ca1b464f2a
Kris Cotter <mrcotter2013@gmail.com>
2018-03-27 16:54:53 +1100

    Try fixing ChromeHeadless disconnect issue when running tests

--- karma.conf.js
+++ karma.conf.js
@@ -14,11 +14,11 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma'),
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
@@ -30,6 +30,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: false,
     browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      "Chrome-headless": {
+        base: 'Chrome',
+        flags: ['--headless', '--no-sandbox']
+      }
+    },
     singleRun: true
   });
 };
